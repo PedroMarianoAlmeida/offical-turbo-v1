b20a017174b4d15a5d669d04b9d17c184b101091
Pedro Almeida <pedroalmeidaprogrammer@gmail.com>
2024-11-20 23:07:26 -0500

    button from shadcn working in package*
    Maybe not the colors from package, from app it is working

--- apps/next-15/next-15-playground/src/app/page.tsx
+++ apps/next-15/next-15-playground/src/app/page.tsx
@@ -1,13 +1,13 @@
 import Image from "next/image";
-import { Card } from "@repo/shadcn/card";
+import { Button } from "@repo/shadcn/button";
 
 export default function Home() {
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Card title="From next" href="">
+        <Button title="From next">
           From Next 15 Playground
-        </Card>
+        </Button>
         <Image
           className="dark:invert"
           src="/next.svg"
