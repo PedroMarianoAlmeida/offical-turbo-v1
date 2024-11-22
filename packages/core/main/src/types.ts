c82f825b041c6ee0f54a781b202be962fa06d21f
Pedro Almeida <pedroalmeidaprogrammer@gmail.com>
2024-11-21 22:53:31 -0500

    add auth types into core

--- packages/core/main/src/types.ts
+++ packages/core/main/src/types.ts
@@ -3,3 +3,23 @@ import { type ReactNode } from "react";
 export interface WithChildren {
   children: ReactNode;
 }
+
+type roleOptions = "admin" | "self" | "authenticated";
+interface UserData {
+  id: number | string;
+  email: string;
+  name: string;
+  photoUrl: string;
+  roles: roleOptions[];
+}
+
+interface SessionWithUser {
+  hasUser: true;
+  userData: UserData;
+}
+
+interface SessionWithoutUser {
+  hasUser: false;
+  userData: null;
+}
+export type CoreSession = SessionWithUser | SessionWithoutUser;
