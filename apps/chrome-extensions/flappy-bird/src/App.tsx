753aee2cb69b5ccaff914fdbfc06cae2e4b753d6
Pedro Almeida <pedroalmeidaprogrammer@gmail.com>
2024-11-25 23:24:57 -0500

    final changes

--- apps/chrome-extensions/flappy-bird/src/App.tsx
+++ apps/chrome-extensions/flappy-bird/src/App.tsx
@@ -3,8 +3,10 @@ import { Game } from "./components/Game";
 
 function App() {
   return (
-    <div className="min-h-screen bg-gray-900 flex flex-col items-center justify-center ">
-      <h1 className="text-4xl font-bold text-white mb-8">Flappy Bird</h1>
+    <div
+      className="min-h-screen bg-gray-900 flex flex-col items-center justify-center"
+      style={{ width: "450px" }}
+    >
       <Game />
       <p className="mt-4 text-gray-400 text-sm">Click or tap to jump</p>
       <p className="text-gray-400 text-sm">
