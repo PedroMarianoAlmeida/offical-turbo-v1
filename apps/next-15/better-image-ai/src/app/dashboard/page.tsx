76c02a54c8129eba8bc8319944b8fa6951d122b7
Pedro Almeida <pedroalmeidaprogrammer@gmail.com>
2024-11-27 00:16:28 -0500

    Remove boilerplate code (and css, I will use from shadcn package)

--- apps/next-15/better-image-ai/src/app/dashboard/page.tsx
+++ apps/next-15/better-image-ai/src/app/dashboard/page.tsx
@@ -1,9 +1,9 @@
 // The email is temporary, just checking if Layout is sending data
 const DashboardPage = () => {
   return (
-    <>
+    <main>
       <h2>Dashboard Page</h2>
-    </>
+    </main>
   );
 };
 
