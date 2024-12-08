7ebc4ee826b629a714fe0194b2c0e97bd81bdc3e
Pedro Almeida <pedroalmeidaprogrammer@gmail.com>
2024-12-08 00:21:53 -0500

    refreshing page when have user
    The SignInWithGoogleButton change do the trick

--- apps/next-15/with-supabase-template/components/auth/SignInWithGoogleButton.tsx
+++ apps/next-15/with-supabase-template/components/auth/SignInWithGoogleButton.tsx
@@ -1,9 +1,29 @@
 "use client";
 import { Button } from "@/components/ui/button";
 import { signInWithGoogle } from "@/lib/auth-actions";
-import React from "react";
+import { createClient } from "@/utils/supabase/client";
+import React, { useEffect, useState } from "react";
+import { useRouter } from "next/navigation";
 
 export const SignInWithGoogleButton = () => {
+  const [user, setUser] = useState<any>(null);
+  const supabase = createClient();
+  const router = useRouter();
+
+  useEffect(() => {
+    const fetchUser = async () => {
+      const {
+        data: { user },
+      } = await supabase.auth.getUser();
+      setUser(user);
+    };
+    fetchUser();
+  }, []);
+
+  useEffect(() => {
+    router.refresh();
+  }, [user]);
+
   return (
     <Button
       type="button"
