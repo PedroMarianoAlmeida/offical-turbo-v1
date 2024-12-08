65981d6c60e4f97f1990777c3351988463e72333
Pedro Almeida <pedroalmeidaprogrammer@gmail.com>
2024-12-08 14:10:36 -0500

    creating next-supabase package

--- apps/next-15/with-supabase-template/app/page.tsx
+++ apps/next-15/with-supabase-template/app/page.tsx
@@ -1,15 +1,13 @@
-import Hero from "@/components/hero";
-import ConnectSupabaseSteps from "@/components/tutorial/connect-supabase-steps";
-import SignUpUserSteps from "@/components/tutorial/sign-up-user-steps";
-import { hasEnvVars } from "@/utils/supabase/check-env-vars";
 import { SignInWithGoogleButton } from "@/components/auth/SignInWithGoogleButton";
 import { UserGreetText } from "@/components/auth/UserGreetText";
+import { add } from "@repo/next-supabase/add";
 
 export default async function Index() {
   return (
     <>
       <main className="flex-1 flex flex-col gap-6 px-4">
         <h2 className="font-medium text-xl mb-4">Next steps</h2>
+        <p>{add(1, 2)}</p>
         <SignInWithGoogleButton />
         <UserGreetText />
       </main>
