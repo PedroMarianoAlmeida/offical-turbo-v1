c9ac53a75d2f904a14e4433e7c511fa31b00c084
Pedro Almeida <pedroalmeidaprogrammer@gmail.com>
2024-12-22 16:31:46 -0500

    fix style bug

--- packages/react/shadcn/src/only-using-collors-and-taiwind/iframe.tsx
+++ packages/react/shadcn/src/only-using-collors-and-taiwind/iframe.tsx
@@ -8,13 +8,12 @@ export const Iframe = ({
   title = "YouTube video player",
 }: IframeProps) => (
   <iframe
-    width="560"
     height="315"
     src={`https://www.youtube.com/embed/${embedUrl}`}
     title={title}
     allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
     referrerPolicy="strict-origin-when-cross-origin"
     allowFullScreen
-    className="border-4 border-ring"
+    className="border-4 border-ring max-w-xl"
   />
 );
