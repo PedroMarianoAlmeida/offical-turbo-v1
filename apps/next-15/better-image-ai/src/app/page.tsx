efc70ad17467f80a25617e168a5b395ca0621b54
Pedro Almeida <pedroalmeidaprogrammer@gmail.com>
2024-12-22 16:10:58 -0500

    add iframe with the workflow on homepage

--- apps/next-15/better-image-ai/src/app/page.tsx
+++ apps/next-15/better-image-ai/src/app/page.tsx
@@ -2,8 +2,8 @@ import Link from "next/link";
 
 import { getCoreServerSession } from "@repo/next-auth/session-adapters";
 import { Button } from "@repo/shadcn/button";
+import { Iframe } from "@repo/shadcn/iframe";
 
-import { ClientHome } from "@/components/page/client-home";
 import { SignInButton } from "@/components/LoginButton";
 
 export default async function Home() {
@@ -11,14 +11,16 @@ export default async function Home() {
   return (
     <main className="flex flex-col gap-4 items-center">
       <h1 className="font-mono">Better Image</h1>
-      <h2>Real Example</h2>
-      <ClientHome />
+      <h2>Check the creator explaining the flow</h2>
+      <Iframe
+        embedUrl="RzEfBKXqp3U?si=Ve7w3cjRt1GFL5s-"
+        title="How to use Better Image AI"
+      />
       <p>
         Generic prompts lead to predictable AI-generated images. Use Better
         Image to craft more detailed and creative prompts, unlocking unique and
         compelling visuals every time!
       </p>
-
       {userData ? (
         <Link href="/dashboard">
           <Button>Give it a try</Button>
