f13eb3a56c7f9ba22e1b29e298a632d6ddffc02a
Pedro Almeida <pedroalmeidaprogrammer@gmail.com>
2024-12-24 22:10:21 -0500

    fix Iframe dimensions

--- packages/react/shadcn/src/only-using-collors-and-taiwind/iframe.tsx
+++ packages/react/shadcn/src/only-using-collors-and-taiwind/iframe.tsx
@@ -8,12 +8,11 @@ export const Iframe = ({
   title = "YouTube video player",
 }: IframeProps) => (
   <iframe
-    height="315"
     src={`https://www.youtube.com/embed/${embedUrl}`}
     title={title}
     allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
     referrerPolicy="strict-origin-when-cross-origin"
     allowFullScreen
-    className="border-4 border-ring max-w-xl"
+    className="border-4 border-ring aspect-[16/9] w-full max-w-5xl"
   />
 );
