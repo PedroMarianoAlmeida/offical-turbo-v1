98ca15a237c5ab131b40dc1bfe79b9b36b073b82
Pedro Almeida <pedroalmeidaprogrammer@gmail.com>
2024-12-28 22:17:28 -0500

    Add the product explanation above the video

--- apps/next-15/better-image-ai/src/app/page.tsx
+++ apps/next-15/better-image-ai/src/app/page.tsx
@@ -12,16 +12,16 @@ export default async function Home() {
   return (
     <main className="flex flex-col gap-4 items-center">
       <h1 className="font-mono">Better Image</h1>
-      <h2>Check the creator explaining the flow</h2>
-      <Iframe
-        embedUrl="RzEfBKXqp3U?si=Ve7w3cjRt1GFL5s-"
-        title="How to use Better Image AI"
-      />
       <p>
         Generic prompts lead to predictable AI-generated images. Use Better
         Image to craft more detailed and creative prompts, unlocking unique and
         compelling visuals every time!
       </p>
+      <h2>Check the creator explaining the flow</h2>
+      <Iframe
+        embedUrl="RzEfBKXqp3U?si=Ve7w3cjRt1GFL5s-"
+        title="How to use Better Image AI"
+      />
       {userData ? (
         <Link href="/dashboard">
           <Button>Give it a try</Button>
