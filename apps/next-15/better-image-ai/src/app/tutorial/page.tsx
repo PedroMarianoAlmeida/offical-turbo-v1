fca319a6caaf8d508ce73ac270b2c25cdacd79fb
Pedro Almeida <pedroalmeidaprogrammer@gmail.com>
2024-12-30 12:38:05 -0500

    update video tutorial

--- apps/next-15/better-image-ai/src/app/tutorial/page.tsx
+++ apps/next-15/better-image-ai/src/app/tutorial/page.tsx
@@ -5,7 +5,7 @@ const Tutorial = () => {
     <main className="flex flex-col gap-4 items-center">
       <h1>Check the creator explaining the flow</h1>
       <Iframe
-        embedUrl="RzEfBKXqp3U?si=Ve7w3cjRt1GFL5s-"
+        embedUrl="JkD8Svvc8yY?si=OsXwM5ykZCXPTlw0"
         title="How to use Better Image AI"
       />
     </main>
