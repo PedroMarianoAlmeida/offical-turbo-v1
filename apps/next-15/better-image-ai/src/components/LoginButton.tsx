ef110d8cd89031981a1bcd50e8d508db893a2ccf
Pedro Almeida <pedroalmeidaprogrammer@gmail.com>
2025-01-02 01:13:36 -0500

    Update LoginButton.tsx

--- apps/next-15/better-image-ai/src/components/LoginButton.tsx
+++ apps/next-15/better-image-ai/src/components/LoginButton.tsx
@@ -3,9 +3,5 @@ import { Button } from "@repo/shadcn/button";
 import { signIn } from "next-auth/react";
 
 export const SignInButton = () => {
-  return (
-    <Button onClick={() => signIn()} variant="outline">
-      Sign in
-    </Button>
-  );
+  return <Button onClick={() => signIn()}>Sign in</Button>;
 };
