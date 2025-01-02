f0b132015d3bf62d8b59f2a579accd42b44af57c
Pedro Almeida <pedroalmeidaprogrammer@gmail.com>
2025-01-02 01:01:32 -0500

    fix build

--- apps/next-15/next-15-playground/src/components/layout-related/LayoutClient.tsx
+++ apps/next-15/next-15-playground/src/components/layout-related/LayoutClient.tsx
@@ -1,19 +1,15 @@
 "use client";
 import { Header } from "@repo/shadcn/header";
-import { useTheme } from "next-themes";
 import { WithChildren } from "@repo/core-main/types";
 import { signIn, signOut } from "next-auth/react";
 import { useCoreSession } from "@repo/next-auth/session-adapters";
 
-
 const LayoutClient = ({ children }: WithChildren) => {
-  const { setTheme } = useTheme();
   const session = useCoreSession();
 
   return (
     <>
       <Header
-        setTheme={setTheme}
         auth={{ session, signIn, signOut }}
         items={{
           publicItems: [
